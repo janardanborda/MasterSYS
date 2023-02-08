7ab64fa3af659ab4d1cebabf4465b7fba38732fa
janardanborda <janardan.borada@syphnosys.com>
2023-02-08 17:58:49 +0530

    normal change

--- app/src/main/java/com/code/master/MainActivity.java
+++ app/src/main/java/com/code/master/MainActivity.java
@@ -8,13 +8,10 @@ import com.code.generalcode.TestToast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String string;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        string = getResources().getString(R.string.app_name);
         new TestToast().showToast(this, "APP NAME");
     }
 }
