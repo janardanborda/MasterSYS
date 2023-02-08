e9296af027c0709e5e4a50dfad10b4b46201b9e2
janardanborda <janardan.borada@syphnosys.com>
2023-02-08 17:38:09 +0530

    No Change

--- app/src/main/java/com/code/master/MainActivity.java
+++ app/src/main/java/com/code/master/MainActivity.java
@@ -1,18 +1,20 @@
 package com.code.master;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.code.generalcode.TestToast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String string;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new TestToast().showToast(this,"APP NAME");
+        string = getResources().getString(R.string.app_name);
+        new TestToast().showToast(this, "APP NAME");
     }
 }
