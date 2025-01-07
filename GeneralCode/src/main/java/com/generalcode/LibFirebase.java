81ff26249eb34ac1419a2199111947b04eea6b8f
janardan <janardan.borada@syphnosys.com>
2025-01-07 18:00:44 +0530

    Testing firebase

---
+++ GeneralCode/src/main/java/com/generalcode/LibFirebase.java
@@ -0,0 +1,7 @@
+package com.generalcode;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+public interface LibFirebase {
+    void LibRemoteConfig(FirebaseRemoteConfig mFirebaseRemoteConfig);
+}
--- app/src/main/java/com/code/master/MainActivity.java
+++ app/src/main/java/com/code/master/MainActivity.java
@@ -1,26 +1,16 @@
 package com.code.master;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.generalcode.TestToast;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new TestToast().showToast(this, "APP NAME");
-
-        findViewById(R.id.toastShow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new TestToast().showToast(MainActivity.this, "APP NAME");
-            }
-        });
     }
 }
