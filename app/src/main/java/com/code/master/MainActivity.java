2a191d18a6d600a95b95dca5967e102d8c401bf8
janardan <janardan.borada@syphnosys.com>
2025-01-08 15:14:46 +0530

    2.2.0

--- app/src/main/java/com/code/master/MainActivity.java
+++ app/src/main/java/com/code/master/MainActivity.java
@@ -5,8 +5,6 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.generalcode.TestToast;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,13 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new TestToast().showToast(this, "APP NAME");
-
-        findViewById(R.id.toastShow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new TestToast().showToast(MainActivity.this, "APP NAME");
-            }
-        });
     }
+    
 }
