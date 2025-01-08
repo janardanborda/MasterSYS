87a356cc759038400c4da3f57c66c7465ba80111
janardan <janardan.borada@syphnosys.com>
2025-01-08 13:40:14 +0530

    2.1.3

--- GeneralCode/src/main/java/com/generalcode/LibFirebase.java
+++
@@ -1,7 +0,0 @@
-package com.generalcode;
-
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-
-public interface LibFirebase {
-    void LibRemoteConfig(FirebaseRemoteConfig mFirebaseRemoteConfig);
-}
